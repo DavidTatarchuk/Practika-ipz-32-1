01920ea93522b00a0efcfe7fba538b032f9ff26f
DavidTatarchuk <skorpion.wormiks@gmail.com>
2026-02-05 01:13:00 +0200

    Add files via upload

---
+++ week 1/task_2.py
@@ -0,0 +1,9 @@
+number = int(input("Введіть число: "))
+
+string_value = f"{number}"
+
+print("Значення:", string_value)
+print("Тип даних:", type(string_value))
+
+res = string_value + "!"
+print("Перевірка (конкатенація):", res)
