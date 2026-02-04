99ec20afa6d695e1757c9c4a27134a9d648b674c
DavidTatarchuk <skorpion.wormiks@gmail.com>
2026-02-05 01:41:40 +0200

    Add files via upload

---
+++ week3/task1.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+
+root = tk.Tk()
+root.title("Перша програма")
+root.geometry("1024x768")
+root.resizable(False, False)
+
+label = tk.Label(root, text="Hello, world!", font=("Arial", 24))
+label.pack(expand=True)
+
+button = tk.Button(root, text="Закрити", command=root.destroy, font=("Arial", 14))
+button.pack(pady=50)
+
+root.mainloop()
