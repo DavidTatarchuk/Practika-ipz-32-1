a3cb99071f6b050088a46312af4142df88d60c38
DavidTatarchuk <skorpion.wormiks@gmail.com>
2026-02-05 01:11:27 +0200

    Add files via upload

---
+++ week 1/task_1.py
@@ -0,0 +1,24 @@
+import this
+
+print("-" * 30)
+
+a = 10
+b = 5.5
+title = "Результат обчислень:"
+
+print(a)
+print(b)
+print(title)
+
+add_res = a + b
+sub_res = a - b
+mul_res = a * 2
+div_res = a / 2
+
+print(title, add_res)
+print(sub_res)
+print(mul_res)
+print(div_res)
+
+user_name = input("Введіть ваше ім'я: ")
+print("Привіт, " + user_name)
